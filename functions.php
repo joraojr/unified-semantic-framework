fd3c19fe4ac8d30712a2d97196b4d9887c4297a6
Cristiano Nascimento <cristianonascimento98@gmail.com>
2020-04-03 11:10:09 -0300

    Função de verificar se é canônica está funcionando

--- functions.php
+++ functions.php
@@ -17,18 +17,32 @@ function printCsv($filename)
     }
 }
 
-function checkLetter($str)
+function checkVowel($str)
 {
     $transLetters = array("á" => "a", "à" => "a", "ã" => "a", "â" => "a", "ä" => "a", "é" => "e", "è" => "e", "ẽ" => "e", "ê" => "e", "ë" => "e", "í" => "i", "ì" => "i", "ĩ" => "i", "î" => "i", "ï" => "i", "ó" => "o", "ò" => "o", "õ" => "o", "ô" => "o", "ö" => "o", "ú" => "u", "ù" => "u", "ũ" => "u", "û" => "u", "ü" => "u", "ç" => "c");
+    $vowels = array("a", "e", "i", "o", "u");
     //Passo tudo pra minúsculo e passo as acentuadas pra não acentuadas e cecidilha para c
     $str = strtolower($str);
     $str = strtr($str, $transLetters);
     
-    if (ctype_alpha($str)) {
+    if (in_array($str, $vowels)) {
         return true;
     } else {
         return false;
     }
+    /*if (ctype_alpha($str)) {
+        return true;
+    } else {
+        return false;
+    }*/
+}
+
+function accented_to_normal($str)
+{
+    $transLetters = array("á" => "a", "à" => "a", "ã" => "a", "â" => "a", "ä" => "a", "é" => "e", "è" => "e", "ẽ" => "e", "ê" => "e", "ë" => "e", "í" => "i", "ì" => "i", "ĩ" => "i", "î" => "i", "ï" => "i", "ó" => "o", "ò" => "o", "õ" => "o", "ô" => "o", "ö" => "o", "ú" => "u", "ù" => "u", "ũ" => "u", "û" => "u", "ü" => "u", "ç" => "c");
+    $str = strtolower($str);
+    $str = strtr($str, $transLetters);
+    return $str;
 }
 
 function setCanonic($filename)
@@ -36,11 +50,39 @@ function setCanonic($filename)
     $cont = 0;
     if (( $handle = fopen( __DIR__ . '/csv/DicionarioFonetico/'.$filename, "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            $num = count($data);
-            if ($cont == 0)
+            
+            if ($cont === 2)
             {
                 $silabas = explode("·", $data[0]);
-                echo $silabas[1];
+                $num = count($silabas);
+                echo $num."<br />\n";
+                $contCanonic = 0;
+                for ($i = 0; $i < $num; $i++)
+                {
+                    $silaba = accented_to_normal($silabas[$i]);
+                    echo $silaba."<br />\n";
+                    $silaba = str_split($silaba);
+                    $qtdLetters = count($silaba);
+                    echo "Quantidade de letras: ".$qtdLetters."<br />\n";
+                    if($qtdLetters === 2)
+                    {
+                        if(checkVowel($silaba[0]) === false)
+                        {
+                            if(checkVowel($silaba[1]) === true)
+                            {
+                                $contCanonic++;
+                            }    
+                        }
+                    }
+                }
+                if($contCanonic === $num)
+                {
+                    echo $data[0]." é canônica";
+                }
+                else
+                {
+                    echo $data[0]." não é canônica";
+                }
             } 
             $cont++; 
         }
@@ -48,7 +90,7 @@ function setCanonic($filename)
     }
 }
 
-checkLetter("cAçárù");
+//checkVowel("A");
 echo "testando função: <br />\n";
 setCanonic($file);
 
